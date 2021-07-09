843724e830550cb7d716df9337798b692fe38c73
2zqa <25235249+2zqa@users.noreply.github.com>
2021-07-09 22:01:31 +0200

    Add FAB transition animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'new_balance_item.dart';
 
@@ -68,6 +69,7 @@ class MoneyManagerApp extends StatelessWidget {
 }
 
 class OverviewPage extends StatelessWidget {
+  final transitionType = ContainerTransitionType.fade;
   final String title;
 
   const OverviewPage({Key? key, required this.title}) : super(key: key);
@@ -75,33 +77,51 @@ class OverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: ListView(
-          padding: EdgeInsets.all(8.0),
-          children: <Widget>[
-            HeadingItem(heading: "Expenses"),
-            BalanceItemCard(Expense(
-                "Spotify",
-                250,
-                RecurringType
-                    .daily)), // TODO: replace single card with list, add heading to list including header argument
-            HeadingItem(heading: "Income"),
-            BalanceItemCard(Income("Salaris", 30000, RecurringType.monthly))
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SecondRoute()),
-            );
-          },
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ));
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(8.0),
+        children: <Widget>[
+          HeadingItem(heading: "Expenses"),
+          BalanceItemCard(Expense(
+              "Spotify",
+              250,
+              RecurringType
+                  .daily)), // TODO: replace single card with list, add heading to list including header argument
+          HeadingItem(heading: "Income"),
+          BalanceItemCard(Income("Salaris", 30000, RecurringType.monthly))
+        ],
+      ),
+      floatingActionButton:
+          CreateNewBalanceItemCardFAB(transitionType: transitionType),
+    );
   }
+}
+
+const double fabSize = 56;
+
+class CreateNewBalanceItemCardFAB extends StatelessWidget {
+  final ContainerTransitionType transitionType;
+
+  const CreateNewBalanceItemCardFAB({Key? key, required this.transitionType})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => OpenContainer(
+        openBuilder: (context, _) => SecondRoute(),
+        closedShape: CircleBorder(),
+        closedColor: Theme.of(context).primaryColor,
+        closedBuilder: (context, openContainer) => Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Theme.of(context).primaryColor,
+          ),
+          height: fabSize,
+          width: fabSize,
+          child: Icon(Icons.add, color: Colors.white),
+        ),
+      );
 }
 
 class HeadingItem extends StatelessWidget {
