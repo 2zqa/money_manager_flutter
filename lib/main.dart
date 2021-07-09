6e2ad7a546886bf09a6760bafea3b5307d27fbd6
2zqa <25235249+2zqa@users.noreply.github.com>
2021-07-09 15:29:09 +0200

    Show card and header

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,46 @@ void main() {
   runApp(MoneyManagerApp());
 }
 
+class BalanceItemCard extends StatefulWidget {
+  const BalanceItemCard({Key? key}) : super(key: key);
+
+  @override
+  _BalanceItemCardState createState() => _BalanceItemCardState();
+}
+
+class _BalanceItemCardState extends State<BalanceItemCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const ListTile(
+            leading: Icon(Icons.album),
+            title: Text('The Enchanted Nightingale'),
+            subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TextButton(
+                child: const Text('BUY TICKETS'),
+                onPressed: () {/* ... */},
+              ),
+              const SizedBox(width: 8),
+              TextButton(
+                child: const Text('LISTEN'),
+                onPressed: () {/* ... */},
+              ),
+              const SizedBox(width: 8),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class MoneyManagerApp extends StatelessWidget {
   final String appName = "Money Manager";
 
@@ -43,12 +83,27 @@ class OverviewPage extends StatelessWidget {
           title: Text(title),
         ),
         body: ListView(
-          children: <Widget>[Text("test1"), Text("test2")],
+          children: <Widget>[
+            HeadingItem(heading: "Expenses"),
+            BalanceItemCard(), // TODO: replace single card with list, add heading to list including header argument
+            HeadingItem(heading: "Income"),
+            BalanceItemCard()
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           tooltip: 'Increment',
           child: Icon(Icons.add),
         ));
+  }
+}
+
+class HeadingItem extends StatelessWidget {
+  final String heading;
+  const HeadingItem({Key? key, required this.heading}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(heading, style: Theme.of(context).textTheme.headline5);
   }
 }
