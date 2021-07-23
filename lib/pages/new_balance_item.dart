25fcd5398a76f2bbc5b3ce15834aa74ee0178553
2zqa <25235249+2zqa@users.noreply.github.com>
2021-07-23 21:27:44 +0200

    Add basic but defunct forms

--- lib/pages/new_balance_item.dart
+++ lib/pages/new_balance_item.dart
@@ -1,18 +1,73 @@
 import 'package:flutter/material.dart';
 
+class NewBalanceItemForm extends StatefulWidget {
+  @override
+  NewBalanceItemFormState createState() {
+    return NewBalanceItemFormState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class NewBalanceItemFormState extends State<NewBalanceItemForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16), // TODO check material spec for padding
+      child: Wrap(
+        // Use wrap so spacing is available
+        spacing: 20,
+        runSpacing: 20,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: "Name",
+              filled: true,
+              border: OutlineInputBorder(),
+            ),
+          ),
+          TextFormField(
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              suffixText: "Euros",
+              filled: true,
+              border: OutlineInputBorder(),
+              labelText: "Amount",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class NewBalanceItemRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: Text('Go back!'),
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("New balance item"),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: "INCOME",
+              ),
+              Tab(
+                text: "EXPENSE",
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            NewBalanceItemForm(),
+            NewBalanceItemForm(),
+          ],
         ),
       ),
     );
