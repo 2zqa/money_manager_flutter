795cd10353f15ad32f1fec3ba6d5c3615282cc25
2zqa <25235249+2zqa@users.noreply.github.com>
2021-08-22 22:18:49 +0200

    Use real FloatingActionButton in transition_widget

--- lib/widgets/transition_widget.dart
+++ lib/widgets/transition_widget.dart
@@ -1,34 +1,35 @@
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
-/// sdfg
+/// A FAB that goes to specified route upon clicking.
 class CreateNewBalanceItemCardFAB extends StatelessWidget {
 
-  /// asdf
+  /// Creates a FAB that goes to [route] with the [transitionType] animation.
   const CreateNewBalanceItemCardFAB(
-      {Key? key, required ContainerTransitionType transitionType, required Widget route})
+      {Key? key,
+      required ContainerTransitionType transitionType,
+      required Widget route})
       : _transitionType = transitionType,
         _route = route,
         super(key: key);
 
-  static const double _fabSize = 56;
   final ContainerTransitionType _transitionType;
   final Widget _route;
 
   @override
-  Widget build(BuildContext context) => OpenContainer(
-        transitionType: _transitionType,
-        closedShape: const CircleBorder(),
-        closedColor: Theme.of(context).primaryColor,
-        openBuilder: (BuildContext context, _) => _route,
-        closedBuilder: (BuildContext context, _) => Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Theme.of(context).primaryColor,
-          ),
-          height: _fabSize,
-          width: _fabSize,
-          child: const Icon(Icons.add, color: Colors.white),
-        ),
-      );
+  Widget build(BuildContext context) {
+    return OpenContainer(
+      transitionType: _transitionType,
+      closedShape: const CircleBorder(),
+      closedColor: Theme.of(context).scaffoldBackgroundColor,
+      openColor: Theme.of(context).scaffoldBackgroundColor,
+      openBuilder: (BuildContext context, _) => _route,
+      closedBuilder: (BuildContext context, VoidCallback openContainer) {
+        return FloatingActionButton(
+          onPressed: () => openContainer(),
+          child: const Icon(Icons.add),
+        );
+      },
+    );
+  }
 }
