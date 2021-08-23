aca6078fe6e41838477f32ae8a20ad3e33932946
2zqa <25235249+2zqa@users.noreply.github.com>
2021-08-24 01:14:44 +0200

    Use muted colors in dark theme, add documentation

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ import 'package:provider/provider.dart';
 import 'pages/overview.dart';
 import 'widgets/balance_item.dart';
 
+/// Runs the app and creates a [ChangeNotifierProvider], used to manage balance items.
+///
+/// See also:
+/// * [BalanceItemList]
 void main() {
   runApp(ChangeNotifierProvider<BalanceItemListModel>(
     create: (BuildContext context) => BalanceItemListModel(),
@@ -14,6 +18,23 @@ void main() {
 
 /// The main widget that gets run from [main].
 class MoneyManagerApp extends StatelessWidget {
+  /// Creates a swatch from a [color]. The primary color shade can be defined
+  /// with [primaryShade], which should be a [MaterialColor] shade (for example: 200)
+  MaterialColor swatchify(MaterialColor color, {int primaryShade = 500}) {
+    return MaterialColor(color[primaryShade]!.value, <int, Color>{
+      50: color[50]!,
+      100: color[100]!,
+      200: color[200]!,
+      300: color[300]!,
+      400: color[400]!,
+      500: color[500]!,
+      600: color[600]!,
+      700: color[700]!,
+      800: color[800]!,
+      900: color[900]!,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,7 +45,11 @@ class MoneyManagerApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      darkTheme: ThemeData.dark(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: swatchify(Colors.green, primaryShade: 200),
+        accentColor: Colors.green.shade200,
+      ),
       themeMode: ThemeMode.system,
       home: const OverviewRoute(),
     );
